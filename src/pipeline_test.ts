0a656581e6801ef97e903c6e2e4882050bd18b27
vfa-thuy <>
2022-06-28 01:26:00 +0700

    Unit test AI model - mask detector on web brower

--- src/pipeline_test.ts
+++ src/pipeline_test.ts
@@ -2,14 +2,15 @@ import * as tf from '@tensorflow/tfjs-core';
 // tslint:disable-next-line: no-imports-from-dist
 import { describeWithFlags, NODE_ENVS } from '@tensorflow/tfjs-core/dist/jasmine_util';
 import * as facetfjs from './index';
-import { FaceEmotionModel, BlazeFaceModel, EmotionPipeline } from './index';
+import { FaceEmotionModel, BlazeFaceModel, Pipeline, FaceMaskModel } from './index';
 import { stubbedImageVals } from './blazeface/test_util';
 import { Prediction } from './pipeline';
 
 describeWithFlags('Pipeline', NODE_ENVS, () => {
   let modelEmotion: FaceEmotionModel;
   let modelFace: BlazeFaceModel;
-  let pipe: EmotionPipeline;
+  let modelMask : FaceMaskModel;
+  let pipe: Pipeline;
   // let originalTimeout: number;
   
   // beforeEach(function () {
@@ -26,28 +27,37 @@ describeWithFlags('Pipeline', NODE_ENVS, () => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000000;
     modelEmotion = await facetfjs.loadFaceEmotions();
     modelFace = await facetfjs.loadBlazeFace();
-    pipe = new EmotionPipeline(modelFace, modelEmotion);
+    modelMask = await facetfjs.loadMask();
+    pipe = new Pipeline(modelFace, modelEmotion, modelMask);
   });
 
   it('infer input and output shape', async () => {
     
 
     const input: tf.Tensor3D = tf.zeros([128, 128, 3]);
+    const inputMask: tf.Tensor3D = tf.zeros([224, 224, 3]);
     // const beforeTensors = tf.memory().numTensors;
     const predicted = await pipe.estimateEmotion(input);
+    const predictedMask = await pipe.estimateMask(inputMask);
     // console.log(predicted.shape);
     expect(predicted).toBeDefined();
+    expect(predictedMask).toBeDefined();
   });
 
   it('estimateFaces returns objects with expected properties', async () => {
     // Stubbed image contains a single face.
     const input: tf.Tensor3D = tf.tensor3d(stubbedImageVals, [128, 128, 3]);
+    const inputMask: tf.Tensor3D = tf.tensor3d(stubbedImageVals, [224, 224, 3]);
     const result = await pipe.estimateEmotion(input);
+    const resultMask = await pipe.estimateMask(inputMask);
 
     const faces = result as Array<Prediction>;
+    const faceMask = resultMask as Array<Prediction>;
 
     // console.log(faces);
     expect(faces).toBeDefined();
+    expect(faceMask).toBeDefined();
+    //expect(facesMask).toBeDefined();
     // expect(faces[0].emotions).toBeDefined();
   });
 
@@ -56,15 +66,18 @@ describeWithFlags('Pipeline', NODE_ENVS, () => {
     // const input: tf.Tensor3D = tf.tensor3d(stubbedImageVals, [128, 128, 3]);
     // const result = await pipe.estimateEmotion(input);
     let emotions = [0.85045341, 0.14814392, 0.00043213543, 0.00056787342, 0.00040270519];
+    let mask = [0.85045341, 0.14814392];
 
     const point = pipe.estimatePoint(emotions);
     const pos = pipe.estimatePositive(emotions);
     const act = pipe.estimateActive(emotions);
-    console.log(pos)
+    const pointMask = pipe.estimatePointMask(mask);
+    //console.log(pos)
 
     expect(point).toBeDefined();
     expect(pos).toBeDefined();
     expect(act).toBeDefined();
+    expect(pointMask).toBeDefined();
     // expect(faces.emotions).toBeDefined();
   });
 
